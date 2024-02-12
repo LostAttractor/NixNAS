d86242a7beaee578cc13f08aae912eca0f5dc434
ChaosAttractor <lostattractor@gmail.com>
2024-02-12 13:42:41 +0800

    services: init

--- configuration/default.nix
+++ configuration/default.nix
@@ -8,6 +8,11 @@
     # Sharing
     ./features/sharing/nfs.nix
     ./features/sharing/samba.nix
+    # Services
+    ./features/services/nginx.nix
+    ./features/services/qbittorrent.nix
+    ./features/services/nextcloud.nix
+    ./features/services/emby.nix
   ];
 
   users = {
@@ -19,6 +24,19 @@
     users.${user} = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC5HypvbsI4xvwfd4Uw7D+SV0AevYPS/nCarFwfBwrMHKybbqUJV1cLM1ySZPxXcZD7+3m48Riiwlssh6o7WM/M= openpgp:0xDE4C24F6" ];
+    };
+  };
+
+  virtualisation.oci-containers.backend = "docker";
+
+  virtualisation.docker = {
+    enable = true;
+
+    daemon.settings = {
+      ipv6 = true;
+      fixed-cidr-v6 = "2001:db8:1::/64";
+      experimental = true;
+      ip6tables = true;
     };
   };
 
