7d7cca81a5e58e6bb27210cad962fdfe7be37a9d
ChaosAttractor <lostattractor@gmail.com>
2024-07-22 11:35:53 +0800

    qbittorrent: using new nixos module

--- configuration/features/services/qbittorrent.nix
+++ configuration/features/services/qbittorrent.nix
@@ -1,23 +1,25 @@
-{ pkgs, user, ... }:
+{ user, config, ... }:
 {
-  systemd.services.qbittorrent-nox = { 
-    description = "qBittorrent-nox service for user ${user}"; 
-
-    wants = ["network-online.target"]; 
-    after = ["local-fs.target" "network-online.target" "nss-lookup.target"]; 
-    wantedBy = ["multi-user.target"];
-
-    serviceConfig = { 
-      Type = "simple";
-      PrivateTmp = "false";
-      User = "${user}"; 
-      ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox"; 
-      TimeoutStopSec = 1800; 
+  services.qbittorrent = {
+    enable = true;
+    webuiPort = 8081;
+    torrentingPort = 9179;
+    user = user;
+    group = config.users.users.${user}.group;
+    serverConfig = {
+      Preferences.WebUI.Password_PBKDF2 = "@ByteArray(5P/EfnMxhqqYzTu5rF//rg==:+tG5V3rzKNOfO/lC5N0g8xeijsgFsqnLdI2sYnK/klhCModR0/1zMoVFv7jmuj3KlsAqV5z92m5rOOiIxYVfZQ==)";
+      BitTorrent.Session.DefaultSavePath = "/mnt/Files/Downloads";
+      RSS = {
+        AutoDownloader.EnableProcessing = true;
+        Session.EnableProcessing = true;
+      };
     };
   };
 
+  networking.firewall.allowedTCPPorts = [ config.services.qbittorrent.torrentingPort ];
+
   services.nginx.virtualHosts."qbittorrent.home.lostattractor.net" = {
-    locations."/".proxyPass = "http://localhost:8080";
+    locations."/".proxyPass = "http://localhost:${toString config.services.qbittorrent.webuiPort}";
     forceSSL = true;
     enableACME = true;
   };
