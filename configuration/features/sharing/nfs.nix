e01a9aa89cec3d7494061ca055ac3a6dc884550b
ChaosAttractor <lostattractor@gmail.com>
2024-07-22 19:25:35 +0800

    nfs: change allowed ip

--- configuration/features/sharing/nfs.nix
+++ configuration/features/sharing/nfs.nix
@@ -3,8 +3,8 @@ _:
   services.nfs.server = {
     enable = true;
     exports = ''
-      /srv/nfs        192.168.0.0/16(rw,fsid=root)                           10.100.0.0/24(rw,fsid=root)
-      /srv/nfs/Files  192.168.0.0/16(rw,all_squash,anonuid=1000,anongid=100) 10.100.0.0/24(rw,all_squash,anonuid=1000,anongid=100)
+      /srv/nfs        10.0.0.0/8(rw,fsid=root)
+      /srv/nfs/Files  10.0.0.0/8(rw,all_squash,anonuid=1000,anongid=100)
     '';
   };
 
